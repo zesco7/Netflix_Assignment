a87f81571fdbd3346aab0654b0908d64cfb6d428
hello <hello>
2022-08-26 23:09:39 +0900

    JACKFLIX 화면구현(오토레이아웃, 기능X)

---
+++ Netflix_Assignment/SignUpViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SignUpViewController.swift
+//  Netflix_Assignment
+//
+//  Created by Mac Pro 15 on 2022/08/26.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var emailOrNumberTextField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var promotionCodeTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        view.backgroundColor = .black
+        
+        emailOrNumberTextField.placeholder = "이메일 주소 또는 전화번호"
+        emailOrNumberTextField.backgroundColor = .systemGray
+        emailOrNumberTextField.layer.cornerRadius = 10
+        emailOrNumberTextField.textAlignment = .center
+        
+        passwordField.placeholder = "비밀번호"
+        passwordField.backgroundColor = .systemGray
+        passwordField.layer.cornerRadius = 10
+        passwordField.textAlignment = .center
+        
+        nicknameTextField.placeholder = "닉네임"
+        nicknameTextField.backgroundColor = .systemGray
+        nicknameTextField.layer.cornerRadius = 10
+        nicknameTextField.textAlignment = .center
+        
+        locationTextField.placeholder = "위치"
+        locationTextField.backgroundColor = .systemGray
+        locationTextField.layer.cornerRadius = 10
+        locationTextField.textAlignment = .center
+        
+        promotionCodeTextField.placeholder = "추천코드 입력"
+        promotionCodeTextField.backgroundColor = .systemGray
+        promotionCodeTextField.layer.cornerRadius = 10
+        promotionCodeTextField.textAlignment = .center
+            
+        signUpButton.setTitle("회원가입", for: .normal)
+        signUpButton.setTitleColor(.black, for: .normal)
+        signUpButton.backgroundColor = .white
+        signUpButton.layer.cornerRadius = 5
+        signUpButton.titleLabel?.textAlignment = NSTextAlignment.center
+        signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+
+        signUpSwitch.onTintColor = .red
+  
+    }
+
+
+ 
+
+}
